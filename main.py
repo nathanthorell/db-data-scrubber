027f0c83d8eb42b3e2376b9c633c3a9ae242338e
nathanthorell <nate@linux.com>
2024-01-30 21:54:09 -0500

    small tweak to table loop

--- main.py
+++ main.py
@@ -27,35 +27,43 @@ conn = pyodbc.connect(conn_str)
 
 # Loop through config mappings
 for map in mappings:
-    current_table_data = td.TableData(
-        schema_name=map["schema"],
-        table_name=map["table"],
-        column_name=map["column"],
-        fake_data_type=map["fake_data_type"],
-    )
+    for column in map["columns"]:
+        current_table_data = td.TableData(
+            schema_name=map["schema"],
+            table_name=map["table"],
+            column_name=column["column_name"],
+            fake_data_type=column["fake_data_type"],
+        )
 
-    # get unique count and db column data type
-    current_table_data.get_unique_count(conn)
-    current_table_data.get_column_data_type(conn)
+        current_name = (
+            f"[{current_table_data.schema_name}].[{current_table_data.table_name}]"
+        )
+        print(f"Starting to process {current_name}")
 
-    # source and fake data
-    if current_table_data.unique_count is not None:
-        fake = current_table_data.create_fake_data(current_table_data.unique_count)
+        # get unique count and db column data type
+        current_table_data.get_unique_count(conn)
+        current_table_data.get_column_data_type(conn)
 
-    current_table_data.get_source_data(conn)
+        # source and fake data
+        if current_table_data.unique_count is not None:
+            fake = current_table_data.create_fake_data(current_table_data.unique_count)
 
-    # now that we have the fake data, create the staging table
-    utils.create_temp_table(conn, current_table_data)
+        current_table_data.get_source_data(conn)
 
-    # map the fake data to the staging table
-    current_table_data.map_source_to_fake()
+        # now that we have the fake data, create the staging table
+        utils.create_temp_table(conn, current_table_data)
 
-    utils.insert_data_into_temp_table(conn, current_table_data)
+        # map the fake data to the staging table
+        current_table_data.map_source_to_fake()
 
-    # Build and execute the merge SQL statement
-    merge_sql = current_table_data.build_merge_sql()
-    if merge_sql:
-        conn.cursor().execute(merge_sql)
-        conn.commit()
+        utils.insert_data_into_temp_table(conn, current_table_data)
+
+        # Build and execute the merge SQL statement
+        merge_sql = current_table_data.build_merge_sql()
+        if merge_sql:
+            conn.cursor().execute(merge_sql)
+            conn.commit()
+
+    print("")
 
 conn.close()
